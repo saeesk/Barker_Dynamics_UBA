a380c3c5bfcf3d813c91a06e65b334ee35bad76d
saeesk <141028913+saeesk@users.noreply.github.com>
2024-10-24 16:24:53 +0530

    Update MLDvsAugUBA_exp.R

--- MLDvsAugUBA_exp.R
+++ MLDvsAugUBA_exp.R
@@ -19,6 +19,43 @@ MLD = function(x0,dt,N)
   }
   return(x)
 }
+####################
+## MLD h(x)= log x
+####################
+MLD1 = function(x0,dt,N) 
+{
+  x = numeric(N)
+  y = numeric(N)
+  x[1] = x0
+  for(i in 1:(N-1))
+  {
+    nu = rnorm(1)
+    y[i]= 1/x[i]
+    y[i+1] = y[i] + dt*(lambda - 2/x[i])*(x[i])^2 + sqrt(2*dt)*nu
+    x[i+1] = y[i+1]
+  }
+  return(x)
+}
+
+f1 = MLD1(x0 = 0.1, dt = 1e-3, N = 1e3)
+x = seq(0,2,length.out = length(f1))
+plot(density(f1), col = 'green',
+     main ='dt = 1e-3, N = 1e3', xlim = c(0,3))
+lines(x,dexp(x, rate = 10))
+
+f2 = MLD1(x0 = 0.1, dt = 1e-3, N = 1e5)
+x = seq(0,2,length.out = length(f2))
+plot(density(f2), col = 'green',
+     main ='dt = 1e-3, N = 1e5', xlim = c(0,3))
+lines(x,dexp(x, rate = 10))
+
+
+f3 = MLD1(x0 = 0.1, dt = 1e-4, N = 5e6)
+x = seq(0,2,length.out = length(f3))
+plot(density(f3), col = 'green',
+     main ='dt = 1e-4, N = 5e6', 
+     xlim = c(0,3), ylim = c(0,lambda))
+lines(x,dexp(x, rate = 10))
 
 ##############################
 ###UBA with Augmented drift 
@@ -69,12 +106,11 @@ Aug_UBA= function(x0,dt,N)
 ###################
 ### Experiments
 ###################
-# pdf('MLDvsUBA2.pdf', width = 10 , height = 10)
-timesteps = c(1e-2,1e-3, 1e-4, 1e-6)
-steps = c(1e5) 
+pdf('MLDvsUBA3.pdf', width = 10 , height = 10)
+timesteps = c(1e-3,1e-4,1e-6)
+steps = c(1e4,1e5,5e6) 
 lambda = 10
-par(mfrow = c(4,3))
-
+par(mfrow = c(3,3))
 
 for( dt in timesteps) {
    for ( N in steps)
@@ -87,8 +123,11 @@ for( dt in timesteps) {
     ratio = r1[1]
     r1 = r1[-1]
     
-    ##MLD
+    ##MLD - entropic map
     r2 = MLD(x0 = 0.1, dt , N)
+    
+    ##MLD - log map 
+    r3 = MLD1(x0 = 0.1, dt, N)
     
     ##Plots
     vec = seq(0,max(r1),by = 0.01)
@@ -97,15 +136,13 @@ for( dt in timesteps) {
          main = paste('dt= ' , dt , ' N= ', N ,'r =' , ratio))
     lines(vec , dexp(vec,rate = lambda))
     lines(density(r2), col = 'blue')
-    legend('topright' , legend = c('AugUBA','MLD', 'Truth') , col = c('red' ,'blue', 'black'), lty = 1)
+    lines(density(r3) , col = 'green')
+    legend('topright' , legend = c('AugUBA','MLD_entr','MLD_log','Truth') , col = c('red' ,'blue','green', 'black'), lty = 1)
 
-    acf(r2, col = "blue", lag.max = 100)
-    lines(acf(r1, plot = FALSE, lag.max = 100)$acf, col = "red")
-
-    plot.ts(r1, col = "red")
-    lines(r2, col = "blue")
+    
   }
 }
-#dev.off()
+dev.off()
+
 
 
